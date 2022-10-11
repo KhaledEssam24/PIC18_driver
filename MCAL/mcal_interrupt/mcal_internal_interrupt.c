e17786f73f782f44ad612e521e327b2d97aa853b
khaledessam24120@gmail.com <khaledessam24120@gmail.com>
2022-10-11 15:08:02 +0200

    first modification

--- MCAL/mcal_interrupt/mcal_internal_interrupt.c
+++ MCAL/mcal_interrupt/mcal_internal_interrupt.c
@@ -40,18 +40,54 @@
 #define TIMER1_INT_LPRIORITY()    (IPR1bits.TMR1IP=0)
 #endif
 #endif
+
+#if INTERRUPT_TIMER2_FEATURE==ENABLE
+#define TIMER2_INT_ENABLE()       (PIE1bits.TMR2IE=1)
+#define TIMER2_INT_DISABLE()      (PIE1bits.TMR2IE=0)
+#define TIMER2_INT_CLEARFLAG()    (PIR1bits.TMR2IF=0)
+#if INTERRUPT_PRIORITY_FEATURE==ENABLE
+#define TIMER2_INT_HPRIORITY()    (IPR1bits.TMR2IP=1)
+#define TIMER2_INT_LPRIORITY()    (IPR1bits.TMR2IP=0)
+#endif
+#endif
+
+#if INTERRUPT_TIMER3_FEATURE==ENABLE
+#define TIMER3_INT_ENABLE()       (PIE2bits.TMR3IE=1)
+#define TIMER3_INT_DISABLE()      (PIE2bits.TMR3IE=0)
+#define TIMER3_INT_CLEARFLAG()    (PIR2bits.TMR3IF=0)
+#if INTERRUPT_PRIORITY_FEATURE==ENABLE
+#define TIMER3_INT_HPRIORITY()    (IPR2bits.TMR3IP=1)
+#define TIMER3_INT_LPRIORITY()    (IPR2bits.TMR3IP=0)
+#endif
+#endif
+
+#if INTERRUPT_CCP1_FEATURE==ENABLE
+#define CCP1_INT_ENABLE()       (PIE1bits.CCP1IE=1)
+#define CCP1_INT_DISABLE()      (PIE1bits.CCP1IE=0)
+#define CCP1_INT_CLEARFLAG()    (PIR1bits.CCP1IF=0)
+#if INTERRUPT_PRIORITY_FEATURE==ENABLE
+#define CCP1_INT_HPRIORITY()    (IPR1bits.CCP1IP=1)
+#define CCP1_INT_LPRIORITY()    (IPR1bits.CCP1IP=0)
+#endif
+#endif
+
+#if INTERRUPT_CCP2_FEATURE==ENABLE
+#define CCP2_INT_ENABLE()       (PIE2bits.CCP2IE=1)
+#define CCP2_INT_DISABLE()      (PIE2bits.CCP2IE=0)
+#define CCP2_INT_CLEARFLAG()    (PIR2bits.CCP2IF=0)
+#if INTERRUPT_PRIORITY_FEATURE==ENABLE
+#define CCP2_INT_HPRIORITY()    (IPR2bits.CCP2IP=1)
+#define CCP2_INT_LPRIORITY()    (IPR2bits.CCP2IP=0)
+#endif
+#endif
 /*************Section macros declarations**********/
+#define HIGH_PRIORITY 1
+#define LOW_PRIORITY  0
 
 /*************Section Data type declaration********/
-typedef enum 
-{
-    HIGH_PRIORITY,
-    LOW_PRIORITY,
-}priority_cfg_t;
+
 /*************Section function declaration*********/
 
 
 
 #endif	/* MCAL_INTERNAL_INTERRUPT_H */
-
-
